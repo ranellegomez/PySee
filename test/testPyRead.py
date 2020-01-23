1769769ad601f9b4b50279d81a9abd927cf9df24
Ranelle Gomez <ranellegomez@berkeley.edu>
2020-01-23 11:08:23 -0800

    Revised the PyRead file and started testPyRead.

--- test/testPyRead.py
+++ test/testPyRead.py
@@ -9,33 +9,26 @@ class TestPyRead(unittest.TestCase):
     # These test cases are still being written.
     def test_english(self):
         curr_directory = sys.path[0]
-        test_img = os.path.join(curr_directory, 'images/English/favoritethings.png')
-        image = Image.open(test_img)
+        favorite_path = os.path.join(curr_directory, 'images/English/favoritethings.png')
+        favorite = Image.open(favorite_path)
         expected = """Staying up for the 61B discussion signup, looking at increased grade bins, writing code: these
 are a few of my favorite things."""
-        print(pr.Scanner.img_to_english(self, image))
-        self.assertEqual(expected, pr.Scanner.img_to_english(self, image))
+        print(pr.Scanner.img_to_english(self, favorite))
+        self.assertEqual(expected, pr.Scanner.img_to_english(self, favorite))
 
     def test_spanish(self):
         curr_directory = sys.path[0]
-        test_img = os.path.join(curr_directory, 'images/Spanish/menudo.png')
-        image = Image.open(test_img)
+        menudo_path = os.path.join(curr_directory, 'images/Spanish/menudo.png')
+        menudo = Image.open(menudo_path)
         expected = 'Me gusta comer menudo a menudo.'
-        print(pr.Scanner.img_to_spanish(self, image))
-        self.assertEqual(expected, pr.Scanner.img_to_spanish(self, image))
+        print(pr.Scanner.img_to_spanish(self, menudo))
+        self.assertEqual(expected, pr.Scanner.img_to_spanish(self, menudo))
 
-        curr_directory = sys.path[0]
-        test_img = os.path.join(curr_directory, 'images/Spanish/menudo.png')
-        image = Image.open(test_img)
-        expected = 'Me gusta comer menudo a menudo.'
-        print(pr.Scanner.img_to_spanish(self, image))
-        self.assertEqual(expected, pr.Scanner.img_to_spanish(self, image))
-
-        test_img_2 = os.path.join(curr_directory, 'images/Spanish/marta.png')
-        image_2 = Image.open(test_img_2)
+        marta_path = os.path.join(curr_directory, 'images/Spanish/marta.png')
+        marta = Image.open(marta_path)
         expected_2 = 'Marta tiene dos marcapasos pero solo tiene un corazón.'
-        print(pr.Scanner.img_to_spanish(self, image))
-        self.assertEqual(expected_2, pr.Scanner.img_to_spanish(self, image_2))
+        print(pr.Scanner.img_to_spanish(self, marta))
+        self.assertEqual(expected_2, pr.Scanner.img_to_spanish(self, marta))
 
     def test_french(self):
         self.assertEqual(True, False)
